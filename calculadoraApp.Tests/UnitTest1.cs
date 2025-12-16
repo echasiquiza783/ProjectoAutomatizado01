b6996dbb364bfc8ec1a45be025dff772899f7798
echasiquiza <echasiquiza5@gmail.com>
2025-12-16 09:21:17 -0500

    proyecto automatizado

---
+++ calculadoraApp.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+using Xunit;
+
+namespace calculadoraApp.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Sumar_DosNumeros_VerificaSumaCorrecta()
+        {
+            var calc = new calculadoraApp.calculadora();
+            var resutado = calc.Sumar(2, 3);
+            Assert.Equal(5, resutado);
+        }
+    }
+
+}
+
