5cd1dd755f75fa9a885710fe363eb2a9f54e8134
echasiquiza <echasiquiza5@gmail.com>
2025-12-16 09:30:30 -0500

    proyecto automatizado consola

---
+++ calculadoraApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+using System;
+namespace calculadoraApp
+{
+    public class calculadora
+    {
+        public int Sumar(int valor1, int valor2) => valor1 + valor2;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new calculadora();
+            Console.WriteLine($"2 + 3 {calc.Sumar(2,3) } ");
+
+        }
+    }
+
+}
